7cbb3f7ec917370eaaf08be26831559961a7f60f
PedroOsimani <pedro.osimani@gmail.com>
2013-05-07 22:09:09 -0300

    Membership

---
+++ Ol4RentAPI/Model/Helpers/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ol4RentAPI.Model.Helpers
+{
+    public class DBHelper
+    {
+        public const string CONN_DEFAULT = "DefaultConnection";
+
+        public static string Connection
+        {
+            get
+            {
+                return CONN_DEFAULT;
+            }
+        }
+    }
+}
